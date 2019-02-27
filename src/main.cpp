abf7829aed265e162e62a3a42b3813c9d0915772
Warit Santaputra <waritsan@gmail.com>
2019-02-27 18:04:55 +0700

    Use wifi manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,17 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 
-const char* ssid = "santaputra";
-const char* password = "1234567890";
-String thingName = WiFi.macAddress(); // 5C:CF:7F:1A:FE:46
+String thingName = WiFi.macAddress();
 const char* host = "dweet.io";
 const int httpPort = 80;
 
 void setup() {
   Serial.begin(9600);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("WiFi connected");
-  Serial.println("(latitude, longitude)");
+  WiFiManager wifiManager;
+  wifiManager.autoConnect();
 }
 
 void loop() {
